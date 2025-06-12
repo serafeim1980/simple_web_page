78db767c8d9e5bc77bbda64c025f82ddff0174e7
Serafim <seraoriginal21@mail.com>
2025-06-12 04:04:31 +0300

    Create columns,image, and description

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+    Hi i am Ardit!i am a Python programmer,teacher and founder of PythonHow.i graduated in 2013 with a master of science in geospatial technologies from the university of Muenster in Germany with a focus on using Python for remote sensing.i have worked with  companies from various countries,such as the Center of Conservation Geography,
+    to map and understand Australian ecosystems,
+    image processing with the Swiss in-Terra,
+    and performing data mining to gain business insights
+    with the Australian rapid intelligence
+    """
+    st.info(content)
