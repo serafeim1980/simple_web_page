426147f124e1b7a83d14a087e60b1d0d21392d4c
Serafim <seraoriginal21@mail.com>
2025-06-13 06:46:53 +0300

    day 22: Added description, url and images

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/photo.png")
@@ -30,9 +30,16 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
